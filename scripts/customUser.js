73689efbe159ddb357d373099ba2ee2fb4b18250
alissoninacio1 <alissoninacio96@gmail.com>
2023-05-19 14:02:15 -0700

    LocalStorage  Initiated

---
+++ scripts/customUser.js
@@ -0,0 +1,21 @@
+const userName = document.querySelector('#userInput'),
+      inputField = document.querySelector('#userInput'),
+      btn = document.querySelector('#btn'),
+      cp = document.querySelector('#customPage')
+
+btn.addEventListener('click', () => {
+
+  cp.innerHTML = 
+  `
+    <p class='user-welcome'>Welcome ${userName.value}!</p>
+  `
+  btn.style.display = 'none'
+  inputField.style.display = 'none'
+
+  try {
+    localStorage.setItem('userName', userName.value);
+  }
+  catch(err) {
+    console.log('Enable you cookies')
+  }
+})
