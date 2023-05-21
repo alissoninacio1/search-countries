0427a2d006277201b176d57417f698d6b655c75e
alissoninacio1 <alissoninacio96@gmail.com>
2023-05-20 17:19:17 -0700

    local storage functionality completed

--- scripts/customUser.js
+++ scripts/customUser.js
@@ -1,23 +1,88 @@
 const userName = document.querySelector('#userInput'),
-      inputField = document.querySelector('#userInput'),
+      // inputField = document.querySelector('#userInput'),
       btn = document.querySelector('#btn'),
       cp = document.querySelector('#customPage')
 
-btn.addEventListener('click', () => {
+//the variable message will take the second argument of the sentence!
+//Because the first one is undefined in the first time the compiler reads it. 
+//(we don't have an item called userName) in the localStorage
+//if the first one exists (after run once), then it will take the
+//localStorage.getItem() value
 
+let message = localStorage.getItem('userName') || ''
+
+//display content function
+const displayInputAndButton  = (value) => {
+  btn.style.display = value
+  userName.style.display = value
+} 
+
+
+//render the userName to the screen
+const  renderUser = (name) => {
   cp.innerHTML = 
   `
-    <p class='user-welcome'>Welcome ${userName.value}!</p>
+    <p class='user-welcome'>Welcome ${name}!</p>
   `
-  btn.style.display = 'none'
-  inputField.style.display = 'none'
+  // btn.style.display = 'none'
+  // inputField.style.display = 'none'
+  displayInputAndButton('none')
+}
 
-  // work in this feature, check if there is a local storage, if not, don't dislpay the fields for input
 
-  try {
-    localStorage.setItem('userName', userName.value);
+
+//if there is a name stored, then the user is rendered and the input is not showed
+if (message) {
+  renderUser(message)
+} 
+//if not, the input is showed and the input will ask to add a userName
+//the event will be available again
+else {
+
+  btn.addEventListener('click', () => {
+
+    renderUser(userName.value)
+    console.log('user name added')
+
+    try {
+      localStorage.setItem('userName', userName.value);
+    }
+    catch(err) {
+      console.log('Enable your cookies')
+    }
+  })
+  
+}
+
+//------change the userName feature ------
+
+//targeting the button
+const btnChange = document.querySelector('#btn-change')
+
+//when the button is clicked, the  localStorage will be cleared.
+//Then, the user will be able to add another name and
+//the p element won't be displayed until a new name be added
+//pass an event as argument in case of preventDefault()
+btnChange.addEventListener('click', (e) => {
+
+  //clear the localStorage to add a new name
+  localStorage.clear()
+  console.log('user elimininated')
+
+  //display input and button again
+  displayInputAndButton('inline')
+  
+  //remove the first child of the custom page div if it exists
+  if (cp.firstElementChild) {
+    cp.firstElementChild.remove()
+  } 
+  //if not, don't allow the event 
+  else {
+    e.preventDefault()
+    alert('There is no user. Please, provide your name!')
   }
-  catch(err) {
-    console.log('Enable you cookies')
-  }
+
+  //restart the input values - assigning the value as undefined
+  userName.value = ''
+
 })
